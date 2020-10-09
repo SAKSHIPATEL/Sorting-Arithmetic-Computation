33b67314e9ac28ed938ccdd0abd2956069756e2c
SAKSHIPATEL <patelsakshi121@gmail.com>
2020-10-09 20:57:57 +0530

    Added : Enter the three numbers

--- sorting.sh
+++ sorting.sh
@@ -1,1 +1,6 @@
-echo "This is Sorting Computation Problem"
+#!/bin/bash -x
+
+read -p "Enter the first number" a
+read -p "Enter the second number" b
+read -p "Enter the third number" c
+echo $a $b $c
